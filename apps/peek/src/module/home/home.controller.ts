72ae99575d165979d3be4b71bad94175c9e56b8a
jm4293 <dlwoals4293@gmail.com>
2025-08-20 23:32:13 +0900

    remove: 불필요 파일 삭제

--- apps/peek/src/module/home/home.controller.ts
+++ apps/peek/src/module/home/home.controller.ts
@@ -2,7 +2,6 @@ import { Response } from 'express';
 
 import { Controller, Get, Res } from '@nestjs/common';
 
-import { ResConfig } from '@peek/config/_res.config';
 import { Public } from '@peek/decorator/public';
 
 import { HomeService } from './home.service';
@@ -16,6 +15,7 @@ export class HomeController {
   async getRecentBoards(@Res() res: Response) {
     const ret = await this.homeService.getRecentBoards();
 
-    return ResConfig.Success({ res, statusCode: 'OK', data: ret });
+    // return ResConfig.Success({ res, statusCode: 'OK', data: ret });
+    return {};
   }
 }
