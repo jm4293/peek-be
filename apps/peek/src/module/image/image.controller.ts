fc5fe679c42bfee5a5dc079f00109273e8085dab
jm4293 <dlwoals4293@gmail.com>
2025-09-18 15:11:34 +0900

    feat: 이미지 다중 업로드 및 p-limit 및 promise.allsettled 사용하여 error 처리

--- apps/peek/src/module/image/image.controller.ts
+++ apps/peek/src/module/image/image.controller.ts
@@ -1,4 +1,5 @@
 import { Response } from 'express';
+import pLimit from 'p-limit';
 
 import {
   BadRequestException,
@@ -9,9 +10,10 @@ import {
   Query,
   Res,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 import { IMAGE_TYPE } from '@peek/constant/image-type';
 import { Public } from '@peek/decorator/public';
@@ -43,6 +45,41 @@ export class ImageController {
     return {
       fileName: ret,
     };
+  }
+
+  @Public()
+  @Post('uploads')
+  @UseInterceptors(FilesInterceptor('images', 10))
+  async uploadFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    if (!files || files.length === 0) {
+      throw new BadRequestException('이미지를 업로드 해주세요.');
+    }
+
+    for (const file of files) {
+      if (file.size > 5 * 1024 * 1024) {
+        throw new BadRequestException(`이미지 파일 크기는 5MB를 초과할 수 없습니다. (파일명: ${file.originalname})`);
+      }
+
+      if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+        throw new BadRequestException(`지원하는 이미지 형식은 JPEG와 PNG입니다. (파일명: ${file.originalname})`);
+      }
+    }
+
+    const limit = pLimit(2);
+
+    const uploadTasks = files.map((file) =>
+      limit(() => this.imageService.uploadImage({ file, type: IMAGE_TYPE.THUMBNAIL })),
+    );
+
+    const fileNames = await Promise.allSettled(uploadTasks);
+
+    const successUploads = fileNames.filter((result) => result.status === 'fulfilled').map((result) => result.value);
+
+    const failUploads = fileNames
+      .filter((result) => result.status === 'rejected')
+      .map((result, index) => files[index].originalname);
+
+    return { successUploads, failUploads };
   }
 
   @Public()
