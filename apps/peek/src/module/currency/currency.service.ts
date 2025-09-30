896036ded62814081445656cecf2395ce139b97c
jm4293 <dlwoals4293@gmail.com>
2025-09-30 14:19:09 +0900

    refactor: currency 리스트 호출 로직 변경

--- apps/peek/src/module/currency/currency.service.ts
+++ apps/peek/src/module/currency/currency.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+import { CurrencyHistory } from '@database/entities/currency';
 import { CurrencyHistoryRepository } from '@database/repositories/currency';
 
 @Injectable()
@@ -7,25 +8,19 @@ export class CurrencyService {
   constructor(private readonly currencyHistoryRepository: CurrencyHistoryRepository) {}
 
   async getCurrencyList() {
-    const query = `
-      SELECT * FROM currency_history ch1
-      WHERE ch1.createdAt = (
-        SELECT MAX(ch2.createdAt) 
-        FROM currency_history ch2 
-        WHERE ch2.curUnit = ch1.curUnit
-      )
-      ORDER BY 
-        CASE ch1.curUnit
-          WHEN 'USD' THEN 1
-          WHEN 'EUR' THEN 2
-          WHEN 'JPY' THEN 3
-          WHEN 'CNH' THEN 4
-        ELSE 5
-      END
-    `;
+    const currencyList = await this.currencyHistoryRepository.find({
+      order: { id: 'DESC' },
+      take: 8,
+    });
 
-    const data = await this.currencyHistoryRepository.query(query);
+    const currencyMap = new Map<string, CurrencyHistory>();
 
-    return { data, total: data.length };
+    currencyList.forEach((currency) => {
+      if (!currencyMap.has(currency.curUnit)) {
+        currencyMap.set(currency.curUnit, currency);
+      }
+    });
+
+    return { data: Array.from(currencyMap.values()), total: currencyMap.size };
   }
 }
