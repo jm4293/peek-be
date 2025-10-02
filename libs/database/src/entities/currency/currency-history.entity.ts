bc835f57d757bea477b8ec3092761e6615aeadc7
jm4293 <dlwoals4293@gmail.com>
2025-10-02 11:22:07 +0900

    refactor: DB | currency-history 컬럼 한국수출입은행 response와 동일하게 변경

--- libs/database/src/entities/currency/currency-history.entity.ts
+++ libs/database/src/entities/currency/currency-history.entity.ts
@@ -4,22 +4,55 @@ import { CurrencyUnitEnum } from '@constant/enum/currency';
 
 import { KoreanTime } from '@database/decorators';
 
+// result: number; // 조회 결과, 1 : 성공, 2 : DATA코드 오류, 3 : 인증코드 오류, 4 : 일일제한횟수 마감
+// cur_unit: string; // 통화코드
+// cur_nm: string; // 국가/통화명
+// ttb: string; // 전신환(송금) 받으실때
+// tts: string; // 전신환(송금) 보내실때
+// deal_bas_r: string; // 매매 기준율
+// bkpr: string; // 장부가격
+// yy_efee_r: string; // 년환가료율
+// ten_dd_efee_r: string; // 10일환가료율
+// kftc_deal_bas_r: string; // 서울외국환중개 매매기준율
+// kftc_bkpr: string; // 서울외국환중개 장부가격
+
 @Entity()
 export class CurrencyHistory {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 3, enum: CurrencyUnitEnum, comment: '통화 코드' })
-  curUnit: CurrencyUnitEnum;
+  @Column({ type: 'varchar', length: 3, nullable: true, enum: CurrencyUnitEnum, comment: '통화 코드' })
+  curUnit: CurrencyUnitEnum | null;
+
+  @Column({ type: 'varchar', length: 10, nullable: true, comment: '국가/통화명' })
+  curNm: string | null;
 
   @Column({ type: 'varchar', length: 10, nullable: true, comment: '통화 단위 설명' })
   curUnitDesc: string | null;
 
-  @Column({ type: 'varchar', length: 10, comment: '국가/통화명' })
-  curNm: string;
+  @Column({ type: 'varchar', length: 10, nullable: true, comment: '전신환(송금) 받으실때' })
+  ttb: string | null;
 
-  @Column({ type: 'varchar', length: 10 })
-  standard: string;
+  @Column({ type: 'varchar', length: 10, nullable: true, comment: '전신환(송금) 보내실때' })
+  tts: string | null;
+
+  @Column({ type: 'varchar', length: 10, nullable: true, comment: '매매 기준율' })
+  dealBasR: string | null;
+
+  @Column({ type: 'varchar', length: 10, nullable: true, comment: '장부가격' })
+  bkpr: string | null;
+
+  @Column({ type: 'varchar', length: 10, nullable: true, comment: '년환가료율' })
+  yyEfeeR: string | null;
+
+  @Column({ type: 'varchar', length: 10, nullable: true, comment: '10일환가료율' })
+  tenDdEfeeR: string | null;
+
+  @Column({ type: 'varchar', length: 10, nullable: true, comment: '서울외국환중개 매매기준율' })
+  kftcDealBasR: string | null;
+
+  @Column({ type: 'varchar', length: 10, nullable: true, comment: '서울외국환중개 장부가격' })
+  kftcBkpr: string | null;
 
   @KoreanTime()
   @CreateDateColumn({ type: 'timestamp' })
