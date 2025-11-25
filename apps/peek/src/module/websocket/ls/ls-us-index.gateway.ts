5f9a1b38e705a69aea1edf9a19d205d5cb0505cb
jm4293 <dlwoals4293@gmail.com>
2025-11-26 00:44:40 +0900

    feat: LS 증권 실시간 필라델피아 반도체 지수 추가

--- apps/peek/src/module/websocket/ls/ls-us-index.gateway.ts
+++ apps/peek/src/module/websocket/ls/ls-us-index.gateway.ts
@@ -9,9 +9,10 @@ import { TokenProvider } from '@libs/shared/const/token';
 
 const TR_CD = 'MK2';
 
-const DOWJONES_TR_KEY = 'DJI@DJI';
-const SP500_TR_KEY = 'SPI@SPX';
-const NASDAQ_TR_KEY = 'NAS@IXIC';
+const DOWJONES_TR_KEY = 'DJI@DJI         ';
+const SP500_TR_KEY = 'SPI@SPX         ';
+const NASDAQ_TR_KEY = 'NAS@IXIC        ';
+const INDEXNASDAQ_TR_KEY = 'USI@SOXX        ';
 
 @WebSocketGateway({
   cors: { origin: ['http://localhost:3000', 'https://stock.peek.run'] },
@@ -28,7 +29,8 @@ export class LsUsIndexGateway implements OnGatewayConnection, OnGatewayDisconnec
 
   private djiIndex = null;
   private spiIndex = null;
-  private nasIndex = null;
+  private nasdaqIndex = null;
+  private phlxSemiconductorIndex = null;
 
   constructor(private readonly securitiesTokenRepository: SecuritiesTokenRepository) {}
 
@@ -42,8 +44,8 @@ export class LsUsIndexGateway implements OnGatewayConnection, OnGatewayDisconnec
     if (this.spiIndex) {
       client.emit(SP500_TR_KEY, this.spiIndex, JSON.parse(JSON.stringify(this.spiIndex.createdAt)));
     }
-    if (this.nasIndex) {
-      client.emit(NASDAQ_TR_KEY, this.nasIndex, JSON.parse(JSON.stringify(this.nasIndex.createdAt)));
+    if (this.nasdaqIndex) {
+      client.emit(NASDAQ_TR_KEY, this.nasdaqIndex, JSON.parse(JSON.stringify(this.nasdaqIndex.createdAt)));
     }
   }
 
@@ -88,9 +90,21 @@ export class LsUsIndexGateway implements OnGatewayConnection, OnGatewayDisconnec
         },
       };
 
+      const messageINDEXNASDAQ = {
+        header: {
+          token: `${this.lsSocketToken}`,
+          tr_type: '3',
+        },
+        body: {
+          tr_cd: TR_CD,
+          tr_key: INDEXNASDAQ_TR_KEY,
+        },
+      };
+
       this.lsSocket.send(JSON.stringify(messageDJI));
       this.lsSocket.send(JSON.stringify(messageSPI));
       this.lsSocket.send(JSON.stringify(messageNAS));
+      this.lsSocket.send(JSON.stringify(messageINDEXNASDAQ));
 
       this.logger.log('웹소켓 LS US 지수 연결 성공');
     };
@@ -98,34 +112,38 @@ export class LsUsIndexGateway implements OnGatewayConnection, OnGatewayDisconnec
     this.lsSocket.onmessage = (event) => {
       const ret = event.data.toString();
 
+      // console.log('ret', ret);
+
       const { header, body } = JSON.parse(ret);
 
-      console.log('LS US INDEX WEBSOCKET MESSAGE:', ret);
+      // console.log('LS US INDEX WEBSOCKET MESSAGE:', ret);
 
       if (body) {
         const { tr_cd, tr_key } = header;
 
-        // if (tr_key === KOSPI_TR_KEY) {
-        //   this.kospiIndex = { ...body, createdAt: new Date() };
+        if (tr_key === DOWJONES_TR_KEY) {
+          this.djiIndex = { ...body, createdAt: new Date() };
+          this.socketServer.emit(DOWJONES_TR_KEY, this.djiIndex, this.djiIndex.createdAt.toISOString());
+        }
 
-        //   this.socketServer.emit(KOSPI_TR_KEY, this.kospiIndex, this.kospiIndex.createdAt.toISOString());
+        if (tr_key === SP500_TR_KEY) {
+          this.spiIndex = { ...body, createdAt: new Date() };
+          this.socketServer.emit(SP500_TR_KEY, this.spiIndex, this.spiIndex.createdAt.toISOString());
+        }
 
-        //   this.stockKoreanIndexHistoryRepository.save({
-        //     ...body,
-        //     type: StockKoreanIndexType.KOSPI,
-        //   });
-        // }
+        if (tr_key === NASDAQ_TR_KEY) {
+          this.nasdaqIndex = { ...body, createdAt: new Date() };
+          this.socketServer.emit(NASDAQ_TR_KEY, this.nasdaqIndex, this.nasdaqIndex.createdAt.toISOString());
+        }
 
-        // if (tr_key === KOSDAQ_TR_KEY) {
-        //   this.kosdaqIndex = { ...body, createdAt: new Date() };
-
-        //   this.socketServer.emit(KOSDAQ_TR_KEY, this.kosdaqIndex, this.kosdaqIndex.createdAt.toISOString());
-
-        //   this.stockKoreanIndexHistoryRepository.save({
-        //     ...body,
-        //     type: StockKoreanIndexType.KOSDAQ,
-        //   });
-        // }
+        if (tr_key === INDEXNASDAQ_TR_KEY) {
+          this.phlxSemiconductorIndex = { ...body, createdAt: new Date() };
+          this.socketServer.emit(
+            INDEXNASDAQ_TR_KEY,
+            this.phlxSemiconductorIndex,
+            this.phlxSemiconductorIndex.createdAt.toISOString(),
+          );
+        }
 
         console.log('tr_cd', tr_cd, 'tr_key', tr_key, 'body', body);
       }
